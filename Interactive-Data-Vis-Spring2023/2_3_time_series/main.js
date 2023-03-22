061ca00c5acb688354753ad1430b472cc33f5702
nphomecare <imrakibul@gmail.com>
2023-03-22 15:45:48 -0400

    Update main.js

--- Interactive-Data-Vis-Spring2023/2_3_time_series/main.js
+++ Interactive-Data-Vis-Spring2023/2_3_time_series/main.js
@@ -1,7 +1,7 @@
  /* CONSTANTS AND GLOBALS */
  const width = window.innerWidth * 0.8,
-  height = 900,
-  margin = { top: 20, bottom: 50, left: 50, right: 20 };
+  height = window.innerHeight * 0.8,
+  margin = { top: 20, bottom: 100, left: 50, right: 20 };
 
 /* LOAD DATA */
 d3.csv('incomeByYear.csv', d => {
