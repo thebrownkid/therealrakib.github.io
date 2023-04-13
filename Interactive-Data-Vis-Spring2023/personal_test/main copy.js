c5a5181fb69f982164e8fd55fb859aa16def57c6
nphomecare <imrakibul@gmail.com>
2023-04-13 16:47:04 -0400

    this is a test directory

---
+++ Interactive-Data-Vis-Spring2023/personal_test/main copy.js
@@ -0,0 +1,71 @@
+/* CONSTANTS AND GLOBALS */
+const width = window.innerWidth * 0.8,
+  height = window.innerHeight * 0.8,
+  margin = { top: 20, bottom: 50, left: 70, right: 20 },
+  radius = 5;
+
+/* LOAD DATA */
+d3.csv("incomeHousing.csv").then(data => {
+  // Parse numeric columns
+  data.forEach(d => {
+    d.year = +d.year;
+    d.income = +d.income;
+    d.downPayment = +d.downPayment;
+  });
+
+  const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // Create scales
+  const xScale = d3.scaleBand()
+    .domain(data.map(d => d.year))
+    .range([0, width])
+    .padding(0.1);
+
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.income + d.downPayment)])
+    .range([height, 0]);
+
+  // Create and append axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis)
+    .selectAll("text")
+    .attr("transform", "rotate(-45)")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .style("text-anchor", "end");
+
+  svg.append("g")
+    .call(yAxis);
+
+  // Add bars for income
+  svg.selectAll(".income-bar")
+    .data(data)
+    .join("rect")
+    .attr("class", "income-bar")
+    .attr("x", d => xScale(d.year))
+    .attr("y", d => yScale(d.income))
+    .attr("width", xScale.bandwidth())
+    .attr("height", d => height - yScale(d.income))
+    .attr("fill", "blue");
+
+  // Add bars for downpayment
+  svg.selectAll(".downpayment-bar")
+    .data(data)
+    .join("rect")
+    .attr("class", "downpayment-bar")
+    .attr("x", d => xScale(d.year))
+    .attr("y", d => yScale(d.income + d.downPayment))
+    .attr("width", xScale.bandwidth())
+    .attr("height", d => yScale(d.income) - yScale(d.income + d.downPayment))
+    .attr("fill", "orange");
+
+});
