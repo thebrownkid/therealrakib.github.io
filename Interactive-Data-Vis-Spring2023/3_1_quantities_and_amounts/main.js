4efd32bda519927d7ced80f0df34386f568cdf9f
nphomecare <imrakibul@gmail.com>
2023-04-13 03:04:46 -0400

    Update main.js

--- Interactive-Data-Vis-Spring2023/3_1_quantities_and_amounts/main.js
+++ Interactive-Data-Vis-Spring2023/3_1_quantities_and_amounts/main.js
@@ -1,6 +1,6 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7;
-height = window.innerHeight;
+const width = window.innerWidth * 0.8;
+height = window.innerHeight * 0.8;
 margin = {top: 20, bottom: 50, left: 60, right: 40};
 radius = 10;
 
