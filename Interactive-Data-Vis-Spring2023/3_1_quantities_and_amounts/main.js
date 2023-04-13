f86209654c653ddb01fe2aac8c7a0fa8c858f89e
nphomecare <imrakibul@gmail.com>
2023-04-13 13:05:54 -0400

    Update main.js

--- Interactive-Data-Vis-Spring2023/3_1_quantities_and_amounts/main.js
+++ Interactive-Data-Vis-Spring2023/3_1_quantities_and_amounts/main.js
@@ -77,7 +77,7 @@ function draw() {
 svg.append("text")
   .attr("class", "axis-label")
   .attr("x", -height/2)
-  .attr("y", -margin.left/2)
+  .attr("y", margin.left/4)
   .attr("transform", "rotate(-90)")
   .attr("fill", "black")
   .attr("text-anchor", "middle")
