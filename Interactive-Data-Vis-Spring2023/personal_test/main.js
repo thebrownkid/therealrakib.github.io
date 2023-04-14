af2cb5c9b8bc6a8b24a4ce044ae2e7a82e9fc70b
nphomecare <imrakibul@gmail.com>
2023-04-13 23:56:59 -0400

    all-content

--- Interactive-Data-Vis-Spring2023/personal_test/main.js
+++ Interactive-Data-Vis-Spring2023/personal_test/main.js
@@ -1,5 +1,5 @@
 /* CONSTANTS AND GLOBALS */
-const width = document.querySelector('.main-content').clientWidth * 0.8;
+const width = document.querySelector('.all-content-center').clientWidth * 0.8;
       height = window.innerHeight * 0.8,
       margin = { top: 20, bottom: 50, left: 70, right: 20 },
       radius = 5;
