5bdf45b821b33167af110776e63ce09341331955
nphomecare <imrakibul@gmail.com>
2023-04-13 23:54:18 -0400

    Why wouldn't this center already?

--- Interactive-Data-Vis-Spring2023/personal_test/main.js
+++ Interactive-Data-Vis-Spring2023/personal_test/main.js
@@ -1,8 +1,8 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.8,
-  height = window.innerHeight * 0.8,
-  margin = { top: 20, bottom: 50, left: 70, right: 20 },
-  radius = 5;
+const width = document.querySelector('.main-content').clientWidth * 0.8;
+      height = window.innerHeight * 0.8,
+      margin = { top: 20, bottom: 50, left: 70, right: 20 },
+      radius = 5;
 
 /* LOAD DATA */
 d3.csv("incomeHousing.csv").then(data => {
