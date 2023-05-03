f52cb85b33be048540529490a43944e06095d7ed
nphomecare <imrakibul@gmail.com>
2023-05-02 23:08:27 -0400

    Update main.js

--- Interactive-Data-Vis-Spring2023/2_1_quantities_and_amounts/main.js
+++ Interactive-Data-Vis-Spring2023/2_1_quantities_and_amounts/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.8,
-  height = window.innerHeight * 0.8,
-  margin = { top: 20, bottom: 50, left: 70, right: 20 };
+const width = window.innerWidth * 0.9;
+const height = 500;
+margin = 50;
 
 /* LOAD DATA */
 d3.csv('squirrelActivities.csv', d3.autoType)
@@ -53,4 +53,3 @@ console.log("data", data)
     .call(yAxis)
 
   })
-
