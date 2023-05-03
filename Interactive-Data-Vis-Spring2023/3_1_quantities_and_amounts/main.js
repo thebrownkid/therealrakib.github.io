631ee4a0ec45dce26774b57e8a879a85a8904b77
nphomecare <imrakibul@gmail.com>
2023-05-03 00:03:38 -0400

    Update main.js

--- Interactive-Data-Vis-Spring2023/3_1_quantities_and_amounts/main.js
+++ Interactive-Data-Vis-Spring2023/3_1_quantities_and_amounts/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.8,
   height = window.innerHeight * 0.8,
-  margin = { top: 20, bottom: 50, left: 70, right: 20 },
+  margin = { top: 20, bottom: 50, left: 50, right: 20 },
   radius = 5;
 
 // // since we use our scales in multiple functions, they need global scope
