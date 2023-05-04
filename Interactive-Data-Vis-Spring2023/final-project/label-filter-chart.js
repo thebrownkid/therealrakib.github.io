607947dc9e28ea339551f1a1a0ef6ee95f8ee58b
nphomecare <imrakibul@gmail.com>
2023-05-04 17:52:27 -0400

    Update label-filter-chart.js

--- Interactive-Data-Vis-Spring2023/final-project/label-filter-chart.js
+++ Interactive-Data-Vis-Spring2023/final-project/label-filter-chart.js
@@ -1,9 +1,11 @@
 function createLabeledChart() {
 // set the dimensions and margins of the graph
-const width = window.innerWidth * 0.8,
-      height = window.innerHeight * 0.8,
-      margin = {top: 20, right: 20, bottom: 50, left: 70},
-      radius = 5;
+
+
+      const width = document.querySelector('.all-content-center').clientWidth * 0.8; // this will help ensure bar chart is centered
+            height = window.innerHeight * 0.5, //reduced height since it looked too long after centering
+            margin = { top: 50, bottom: 50, left: 70, right: 70 },
+            radius = 5;
 
 // append the svg object to the body of the page
 const svg = d3.select("#my_dataviz")
