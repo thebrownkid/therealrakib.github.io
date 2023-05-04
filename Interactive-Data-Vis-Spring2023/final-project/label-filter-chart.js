caf7532ceff2436598b473407536b792b3d63774
nphomecare <imrakibul@gmail.com>
2023-05-04 19:33:15 -0400

    Update label-filter-chart.js

--- Interactive-Data-Vis-Spring2023/final-project/label-filter-chart.js
+++ Interactive-Data-Vis-Spring2023/final-project/label-filter-chart.js
@@ -164,17 +164,17 @@ function mouseover() {
 
     //note about the pink area chart
     svg.append("text")
-    .attr("x", width - 400)
-    .attr("y", 350)
+    .attr("x", width - 300)
+    .attr("y", 370)
     .text("Pink area chart represents 20% Down Payment")
     .style("font-weight", "bold")
     .style("font-size", 10)
     .append("tspan")
-      .attr("x", width - 400)
+      .attr("x", width - 300)
       .attr("dy", "1.2em")
       .text("Lines above this chart represents higher affordability")
       .append("tspan")
-      .attr("x", width - 400)
+      .attr("x", width - 300)
       .attr("dy", "1.2em")
       .text("Lines inside this chart represents lower affordability")
     
