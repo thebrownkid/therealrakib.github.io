58694a503ff9e25f5536d733856d8990bfd8bb8e
nphomecare <imrakibul@gmail.com>
2023-05-04 16:07:33 -0400

    Update main.js

--- Interactive-Data-Vis-Spring2023/3_2_distributions/main.js
+++ Interactive-Data-Vis-Spring2023/3_2_distributions/main.js
@@ -99,15 +99,15 @@ function drawChart(filterData){
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Income");
+    //svg.append("g")
+        //.attr("class", "y axis")
+        //.call(yAxis)
+      //.append("text")
+        //.attr("transform", "rotate(-90)")
+        //.attr("y", 6)
+        //.attr("dy", ".71em")
+        //.style("text-anchor", "end")
+        //.text("Income");
 
 
            //tooltip
